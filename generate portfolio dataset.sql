e7d68c106ab0721308ac1e4d30465e56bc83dcf4
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-07-22 13:18:20 +0100

    Added table for prediction values

--- generate portfolio dataset.sql
+++ generate portfolio dataset.sql
@@ -7,3 +7,9 @@ SELECT 	h.portfolioCode
 FROM 	pData.portfolioHoldings h
 ,		pData.instrumentTypes t
 WHERE	h.instrumentTypeId = t.instrumentTypeId
+AND		h.valueDate = 
+(		SELECT	Max(h2.valueDate)
+		FROM	portfolioCode h2
+		WHERE	h.portfolioCode = h2.portfolioCode
+		AND		h.dsCode = h2.dsCode
+		AND		h2.valueDate >= STR_TO_DATE('01,5,2016','%d,%m,%Y'));
